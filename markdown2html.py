6b3655334170341211b8152a95a57949b5272b46
David Muindi <muindidiego@gmail.com>
2023-10-21 10:15:20 +0000

    Tasks

--- markdown2html.py
+++ markdown2html.py
@@ -4,8 +4,9 @@ markdown2html
 This module receives two arguments: The input markdown (.md) and the output html (.html).
 """
 
-import os
 import sys
+import re
+import os
 
 def markdown2html(md, html):
     if not os.path.isfile(md):
@@ -26,62 +27,53 @@ def markdown2html(md, html):
                 if inside_paragraph:
                     html_str.append('</p>\n')
                     inside_paragraph = False
-                html_str.append('<ul>\n')
+                html_str.append('<ul>\n')  
                 inside_list = True
             count = line.count('-')
             text = line[count:].strip()
-            html_str.append('<li>{}</li>\n'.format(parse_markdown(text)))
+            text = re.sub(r'__([^__]*)__', r'<em>\1</em>', text)  # Parsing emphasis syntax
+            text = re.sub(r'\*\*([^**]*)\*\*', r'<b>\1</b>', text)  # Parsing bold syntax
+            html_str.append('<li>{}</li>\n'.format(text))
         elif line.startswith('*'):
             if not inside_order_list:
                 if inside_paragraph:
                     html_str.append('</p>\n')
                     inside_paragraph = False
-                html_str.append('<ol>\n')
+                html_str.append('<ol>\n')  
                 inside_order_list = True
             count = line.count('*')
             text = line[count:].strip()
-            html_str.append('<li>{}</li>\n'.format(parse_markdown(text)))
+            html_str.append('<li>{}</li>\n'.format(text))
         else:
             if inside_list:
-                html_str.append('</ul>\n')
+                html_str.append('</ul>\n') 
                 inside_list = False
             if inside_order_list:
-                html_str.append('</ol>\n')
+                html_str.append('</ol>\n') 
                 inside_order_list = False
             if line.strip() != '':
                 if not inside_paragraph:
-                    html_str.append('<p>\n')
+                    html_str.append('<p>\n') 
                     inside_paragraph = True
                 text = line.strip()
-                html_str.append('{}<br/>\n'.format(parse_markdown(text)))
+                text = re.sub(r'__([^__]*)__', r'<em>\1</em>', text)  # Parsing emphasis syntax
+                text = re.sub(r'\*\*([^**]*)\*\*', r'<b>\1</b>', text)  # Parsing bold syntax
+                html_str.append('{}<br/>\n'.format(text))
             else:
                 if inside_paragraph:
                     html_str = html_str[:-1] # Remove the last <br/>
-                    html_str.append('</p>\n')
+                    html_str.append('</p>\n') 
                     inside_paragraph = False
 
-    if inside_list:
+    if inside_list: 
         html_str.append('</ul>\n')
-    if inside_order_list:
+    if inside_order_list: 
         html_str.append('</ol>\n')
     if inside_paragraph:
-        html_str = html_str[:-1]
+        html_str = html_str[:-1] 
         html_str.append('</p>\n')
 
     with open(html, 'w') as f:
         f.writelines(html_str)
 
     return 0
-
-def parse_markdown(text):
-    text = text.replace('**', '<b>').replace('__', '<em>').replace('**', '</b>').replace('__', '</em>')
-    return text
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python markdown2html.py input.md output.html", file=sys.stderr)
-        sys.exit(1)
-    md_file = sys.argv[1]
-    html_file = sys.argv[2]
-    markdown2html(md_file, html_file)
-
