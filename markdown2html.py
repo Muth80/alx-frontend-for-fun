f19362e899a71acc70e1d3b9981891af27881378
David Muindi <muindidiego@gmail.com>
2023-10-21 09:23:26 +0000

    Tasks

--- markdown2html.py
+++ markdown2html.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-import os
+"""markdown2html
+This module receives two arguments: The input markdown (.md) and the output html (.html).
+"""
 import sys
-import re
+import os
 
-def parse_heading(line):
-    level = len(line) - len(line.lstrip('#'))
-    line = line.lstrip('#').strip()
-    return '<h' + str(level) + '>' + line + '</h' + str(level) + '>' + '\n'
+def markdown2html(md, html):
+    if not os.path.isfile(md):
+        print("Missing " + md, file=sys.stderr)
+        return 1
+        
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        return 1
+    md = sys.argv[1]
+    html = sys.argv[2]
+    return markdown2html(md, html)
 
-if len(sys.argv) != 3:
-    print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-    sys.exit(1)
-
-input_file = sys.argv[1]
-output_file = sys.argv[2]
-
-# Checking if the file exists
-if not os.path.exists(input_file):
-    print('Missing ' + input_file, file=sys.stderr)
-    sys.exit(1)
-
-# Open input and output files
-with open(input_file, 'r') as in_file, open(output_file, 'w') as out_file:
-    for line in in_file.readlines():
-        line = line.strip()
-        if re.search('^#{1,6}\s', line):
-            out_file.write(parse_heading(line))
-
-# If everything is good
-sys.exit(0)
+if __name__ == "__main__":
+    exit(main())
